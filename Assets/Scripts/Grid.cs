7dbee9ec9a2c716b94d4d502e0865209ca5cc577
Dunefarm <smokingwolf@gmail.com>
2017-05-16 23:03:48 +0200

    Tried to change the grid. I don't know if this square look without a physical grid is good - but it looks kinda clean. Next up, I think I should look into displaying cards by hovering over them. Thereby I can make the cards in the shop smaller. (If the game were for mobile, I'd probably have it so you'd have to tap on the card to display it - like in the old version).

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
+    public GameObject GridDividerPrefab;
+
     public Transform UpperLeftCorner;
     public Transform LowerRightCorner;
     public int GridSize = 8;
+    public float CellSize = 1;
+
+    private Transform _transform;
+
+    void Awake()
+    {
+        _transform = transform;
+        UpperLeftCorner.gameObject.SetActive(false);
+        LowerRightCorner.gameObject.SetActive(false);
+        //CreateGrid();
+    }
 
     public Vector3 GridCoordinateToVector3(Finite2DCoord coord)
     {
@@ -17,5 +30,43 @@ public class Grid : MonoBehaviour {
         float x = Mathf.Lerp(UpperLeftCorner.position.x, LowerRightCorner.position.x, i / (float)(GridSize - 1));
         float y = Mathf.Lerp(UpperLeftCorner.position.y, LowerRightCorner.position.y, j / (float)(GridSize - 1));
         return new Vector3(x, y, 0);
+    }
+
+    public void CreateGrid()
+    {
+        float gridDividerThickness = GridDividerPrefab.transform.localScale.x * 0.5f;
+        float dividerSpace = gridDividerThickness + CellSize;
+
+        LowerRightCorner.position = UpperLeftCorner.position + new Vector3(GridSize * CellSize, -GridSize * CellSize, 0);
+        Vector3 startPos = UpperLeftCorner.position + new Vector3(-1, 1, 0) * CellSize * 0.5f;
+
+        return;
+
+        float rowLength = CellSize * (GridSize + 1);
+        Vector3 rowPos = startPos + Vector3.right * (rowLength * 0.5f);
+        
+
+        float columnHeight = CellSize * (GridSize + 1);
+        Vector3 columnPos = startPos + Vector3.down * (columnHeight * 0.5f);
+
+        Vector3 rowEnd = rowPos + Vector3.down * columnHeight;
+        Transform rowEndObj = (Instantiate(GridDividerPrefab, rowEnd, Quaternion.identity, _transform) as GameObject).transform;
+        rowEndObj.name = "RowEnd";
+
+        GameObject startPosObj = Instantiate(GridDividerPrefab, startPos, Quaternion.identity, _transform) as GameObject;
+        startPosObj.name = "Start pos";
+
+        for (int i = 0; i < GridSize + 1; i++)
+        {
+            Vector3 pos = Vector3.Lerp(rowPos, rowEnd, (float)i / (float)GridSize);
+            Transform row = (Instantiate(GridDividerPrefab, pos, Quaternion.identity, _transform) as GameObject).transform;
+            row.localScale = new Vector3(rowLength, row.localScale.y, row.localScale.z);
+            row.name = "GridRow" + (i + 1);
+        }
+        for (int j = 0; j < GridSize + 1; j++)
+        {
+            Transform column = (Instantiate(GridDividerPrefab, columnPos + Vector3.right * j * CellSize, Quaternion.identity, _transform) as GameObject).transform;
+            column.localScale = new Vector3(column.localScale.x, columnHeight, column.localScale.z);
+        }
     }
 }
