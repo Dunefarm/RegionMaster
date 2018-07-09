9321fca5ade6688bdaa7721f19049d41a4d724e2
Dunefarm <smokingwolf@gmail.com>
2018-07-09 16:41:42 +0200

    CA_AddDamage and CA_Heal can now use a CAC to modify their values. Two new cards utilizes this!

--- Assets/Scripts/Cards/Abilities/CA_AddDamage.cs
+++ Assets/Scripts/Cards/Abilities/CA_AddDamage.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using ExtendedEditor;
 
 public class CA_AddDamage : CardAbility
 {
 
     public int Damage;
+    public bool MultiplyByCounter = false;
+    [ShowIf("MultiplyByCounter")]
+    public CardAbilityCounter CACounter;
 
     public override void ActivateAbility()
     {
-        EventManager.AddDamageToPool(Damage);
+        EventManager.AddDamageToPool(Damage * (MultiplyByCounter ? CACounter.Value : 1));
         ResolveAbility();
     }
 }
--- Assets/Scripts/Cards/Abilities/CA_Heal.cs
+++ Assets/Scripts/Cards/Abilities/CA_Heal.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using ExtendedEditor;
 
 public class CA_Heal : CardAbility
 {
 
     public int HealAmount = 2;
 
+    public bool MultiplyByCounter = false;
+    [ShowIf("MultiplyByCounter")]
+    public CardAbilityCounter CACounter;
+
+    protected int _amount = 0;
+
     public override void ActivateAbility()
     {
-        EventManager.HealPlayer(HealAmount, Player.GetCurrentPlayer);
+        _amount = HealAmount * (MultiplyByCounter ? CACounter.Value : 1);
+        HealAndResolve(_amount);
+    }
+
+    protected void HealAndResolve(int amountToHeal)
+    {
+        EventManager.HealPlayer(amountToHeal, Player.GetCurrentPlayer);
         ResolveAbility();
     }
 }
