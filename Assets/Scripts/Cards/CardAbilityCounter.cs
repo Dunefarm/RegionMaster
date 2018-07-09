10a4a37211699e45c2e5aa173c7ff03e90e24681
Dunefarm <smokingwolf@gmail.com>
2018-07-09 16:39:24 +0200

    Added a new script type: Card Ability Counters (CAC). These can be used to change numbers in CAs by counting set properties. New CAC: TokensMarked. This can count marked tokens in the grid, based on ownership and color.

---
+++ Assets/Scripts/Cards/CardAbilityCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAbilityCounter : MonoBehaviour
+{
+    public int Value
+    {
+        get { return Count(); }
+    }
+
+    protected int _value = 0;
+
+    public virtual int Count()
+    {
+        return _value;
+    }
+}
---
+++ Assets/Scripts/Cards/Counters/CAC_TokensMarked.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAC_TokensMarked : CardAbilityCounter
+{
+    public enum PlayerOwner { CurrentPlayer, Opponent}
+    public enum CountColorsType { Zero, One, Two}
+
+    public bool CountOwned = false;
+    public PlayerOwner OwnedBy = PlayerOwner.CurrentPlayer;
+    public CountColorsType CountColors = CountColorsType.Zero;
+    public Token.ColorType FirstColor = Token.ColorType.Red;
+    public Token.ColorType SecondColor = Token.ColorType.Green;
+
+
+    public override int Count()
+    {
+        if(CountOwned)
+        {
+            Player targetPlayer = (OwnedBy == PlayerOwner.CurrentPlayer ? Player.GetCurrentPlayer : Player.GetCurrentOpponent);
+            if (CountColors == CountColorsType.Zero)
+                _value = GridManager.Count.Tokens(targetPlayer);
+            else if(CountColors == CountColorsType.One)
+                _value = GridManager.Count.Tokens(targetPlayer, FirstColor);
+            else
+                _value = GridManager.Count.Tokens(targetPlayer, FirstColor, SecondColor);
+        }
+        else
+        {
+            if (CountColors == CountColorsType.One)
+                _value = GridManager.Count.Tokens(FirstColor);
+            else
+                _value = GridManager.Count.Tokens(FirstColor, SecondColor);
+        }
+        return _value;
+    }
+
+}
